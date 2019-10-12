031b370b205620409f229fa0c9d192fb6c9324dd
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2019-10-12 20:26:16 +0200

    message slack fix

--- src/App/Slack/Permission/PermissionChecker.php
+++ src/App/Slack/Permission/PermissionChecker.php
@@ -26,7 +26,7 @@ class PermissionChecker
         if (false === in_array($user, $this->usersAllowed, false)) {
             $message = <<<STRING
  Sorry :hear_no_evil:, I'm programmed to do not follow orders from strangers.
-> Maybe you can ask someone to add your id `{$user}` on the allowed users list ?
+>Maybe you can ask someone to add your id `{$user}` on the allowed users list ?
 STRING;
 
             throw new UserNotAllowedException($message);
@@ -41,7 +41,7 @@ STRING;
         if (false === in_array($channel, $this->channelsAllowed, false)) {
             $message = <<<STRING
  Sorry :speak_no_evil:, I'm not allowed to operate on this channel.
- > Maybe you can ask someone to add this channel id `{$channel}` on the allowed channels list ?
+ >Maybe you can ask someone to add this channel id `{$channel}` on the allowed channels list ?
 STRING;
             throw new ChannelNotAllowedException($message);
         }
